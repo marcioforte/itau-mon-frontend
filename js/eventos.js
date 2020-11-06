86ab682a84dcaf93d63af0015b326a53de718af3
Marcelo Martins <marcelonam@gmail.com>
2020-11-06 15:34:23 -0300

    .

--- js/eventos.js
+++ js/eventos.js
@@ -101,22 +101,23 @@ function downloadCSV(csv, filename) {
 function CriaPDF() {
     var minhaTabela = document.getElementById("tabela").innerHTML;
     var style = "<style>";
-    style = style + "table {width: 100%;font: 20px Calibri;}";
+    style = style + "table {width: 95%;}";
     style = style + "table, th, td {border: solid 1px #DDD; border-collapse: collapse;";
     style = style + "padding: 2px 3px;text-align: center;}";
     style = style + "</style>";
+    
     // CRIA UM OBJETO WINDOW
     var win = window.open('', '', 'height=700,width=700');
     win.document.write('<html><head>');
-    win.document.write('<title>Relatório de Eventos</title>');   // <title> CABEÇALHO DO PDF.
-    win.document.write(style);                                     // INCLUI UM ESTILO NA TAB HEAD
+    win.document.write('<title>Relatório de Eventos</title>');   
+    win.document.write(style);
     win.document.write('</head>');
     win.document.write('<body>');
     win.document.write('<br>');
     win.document.write('<center><font color="#4B515D" face="Verdana, Geneva, sans-serif"><h3>Relatório de eventos por período</h3><center>');
-    win.document.write(minhaTabela);                          // O CONTEUDO DA TABELA DENTRO DA TAG BODY
+    win.document.write(minhaTabela);                          
     win.document.write('</body></html>');
-    win.document.close();                                            // FECHA A JANELA
-    win.print();                                                            // IMPRIME O CONTEUDO
+    win.document.close();                                            
+    win.print();                                                           
 }
 
